265c5e1c7bb6b38a0f316397dda5a12076c29956
Riza Nafis <riza.nafis@gmail.com>
2019-07-16 21:28:14 -0700

    Adding verification stage

--- verifikasi_sertifikat/views.py
+++ verifikasi_sertifikat/views.py
@@ -1,3 +1,17 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 
+from .models import Sertifikat
+
+
 # Create your views here.
+def verifikasi(request):
+    if request.method == "POST":
+        kode = request.POST['kode']
+        try:
+            sertifikat = Sertifikat.objects.get(kode=kode)
+            context = {'kode': kode, 'valid': True, 'sertifikat': sertifikat}
+        except ObjectDoesNotExist:
+            context = {'kode': kode, 'valid': False}
+        return render(request, 'verifikasi_sertifikat/sertifikat_result.html', context)
+    return render(request, 'verifikasi_sertifikat/sertifikat_result.html')
